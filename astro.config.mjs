29c55bf3800edffd310f6fad9727ba3ecf0b2951
Peter <storm.sk123@gmail.com>
2023-03-27 22:42:02 +0200

    added deno deploy, deployed. Added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import deno from "@astrojs/deno";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: deno({
+    port: 8000,
+    hostname: "localhost"
+  }),
+  integrations: [tailwind()]
+});
